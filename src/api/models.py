6a8c0d09a74476cdf62d5c130ff3efeab43f5a0b
Ian Garvey <ianarthurgarvey@gmail.com>
2025-09-30 22:32:13 +0000

    created models and authentication endpoints for sign up and login. Debating on detailing relationships in models.

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,121 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, ForeignKey, DateTime, Numeric
 from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime, timezone
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False, index=True)
+    password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean(), default=True)
 
+    # Method to hash a password
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    # Method to verify a password
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+    
+class Category(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
+        }
+    
+class Product(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    category_id: Mapped[int] = mapped_column(ForeignKey("category.id"), index=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    price: Mapped[float] = mapped_column(Numeric(10, 2), nullable=False)
+    description: Mapped[str] = mapped_column(db.Text)
+    image: Mapped[str] = mapped_column(db.Text)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "category_id": self.category_id,
+            "title": self.title,
+            "price": float(self.price),
+            "description": self.description,
+            "image": self.image
+        }
+
+    
+class Cart(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), index=True)
+
+    def serialize(self): 
+        return {
+            "id": self.id,
+            "user_id": self.user_id
+        }
+
+class CartItem(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    cart_id: Mapped[int] = mapped_column(ForeignKey("cart.id"), index=True)
+    product_id: Mapped[int] = mapped_column(ForeignKey("product.id"), index=True)
+    quantity: Mapped[int] = mapped_column(default=1, nullable=False)
+    added_at: Mapped[datetime] = mapped_column(DateTime, default=lambda: datetime.now(timezone.utc))
+
+class Order(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), index=True)
+    cart_id: Mapped[int] = mapped_column(ForeignKey("cart.id"), index=True)
+    total: Mapped[float] = mapped_column(Numeric(10, 2), nullable=False)
+    status: Mapped[str] = mapped_column(String(50), default="Order has been placed!", nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=lambda: datetime.now(timezone.utc))
+    shipped_at: Mapped[datetime] = mapped_column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=True)
+    phone_number: Mapped[str] = mapped_column(String(20))
+    address: Mapped[str] = mapped_column(String(500), nullable=False)
+    city: Mapped[str] = mapped_column(String(50), nullable=False)
+    state: Mapped[str] = mapped_column(String(50), nullable=False)
+    zip_code: Mapped[str] = mapped_column(String(20), nullable=False)
+
+    def serialize(self): 
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "cart_id": self.cart_id,
+            "total": float(self.total),
+            "status": self.status,
+            "created_at": self.created_at.isoformat(),
+            "shipped_at": self.shipped_at.isoformat() if self.shipped_at else None,
+            "phone_number": self.phone_number,
+            "address": self.address,
+            "city": self.city,
+            "state": self.state,
+            "zip_code": self.zip_code
+        }
+    
+class OrderItem(db.Model): 
+    id: Mapped[int] = mapped_column(primary_key=True)
+    order_id: Mapped[int] = mapped_column(ForeignKey("order.id"), index=True)
+    product_id: Mapped[int] = mapped_column(ForeignKey("product.id"), index=True)
+    quantity: Mapped[int] = mapped_column(default=1, nullable=False)
+    price_at_purchase: Mapped[float] = mapped_column(Numeric(10, 2), nullable=False)
+
+    def serialize(self): 
+        return {
+            "id": self.id,
+            "order_id": self.order_id,
+            "product_id": self.product_id,
+            "quantity": self.quantity,
+            "price_at_purchase": self.price_at_purchase
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -5,18 +5,89 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import jwt_required, get_jwt_identity, create_access_token, JWTManager 
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
+from dotenv import load_dotenv
 
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
 
+# health check endpoint
+@api.route('/hello', methods=['GET'])
+def hello():
+    return jsonify({"message": "Hello from the API!"}), 200
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/signup', methods=['POST'])
+def create_user():
+    data = request.get_json()
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+    # valid input
+    if not data or 'email' not in data or 'password' not in data:
+        return jsonify({"error": "Email and password are required"}), 400 
+    
+    # check to see if user already exists
+    if User.query.filter_by(email=data['email']).first():
+        return jsonify({"error": "User already exists"}), 400
+    
+    # create new user
+    hashed_password = generate_password_hash(data['password'])
+    new_user = User(
+        email=data['email'],
+        password=hashed_password,
+        is_active=True
+    )
 
-    return jsonify(response_body), 200
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({"message": "User created successfully"}), 201
+    except Exception as e: 
+        db.session.rollback()
+        return jsonify({"error": str(e)}), 500
+
+@api.route('/login', methods=['POST'])
+def create_token():
+    data = request.get_json()
+
+    if not data or 'email' not in data or 'password' not in data:
+        return jsonify({"error": "Email and password are required"}), 400
+    
+    user = User.query.filter_by(email=data['email']).first()
+
+    if not user or not check_password_hash(user.password, data['password']):
+        return jsonify({"error": "invalid credentials"}), 401
+
+    # create token
+    access_token = create_access_token(identity=str(user.id))
+    return jsonify({
+        "token": access_token,
+        "user_id": str(user.id),
+        "email": user.email
+    }), 200
+
+@api.route('/logout', methods=['POST'])
+@jwt_required()
+def logout():
+
+    return jsonify({"message": "Logged out successfully"}), 200
+
+# private route to check token and see if authorized to view private page
+@api.route('/private', methods=['GET'])
+@jwt_required()
+def private():
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+    
+    return jsonify({
+        "message": "The private page works!",
+        "user": {
+            "id": str(user.id),
+            "email": user.email
+        }
+    }), 200
