b9836eb7ac71a3de5241ebaed8b81d614712cbb0
Ian Garvey <ianarthurgarvey@gmail.com>
2025-10-09 01:06:59 +0000

    changed carousel to temporarily include navigation arrows to cycle through images

--- src/front/components/Carousel.jsx
+++ src/front/components/Carousel.jsx
@@ -7,21 +7,48 @@ export const Carousel = () => {
 
     return (
 
-        <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
-            <div className="carousel-inner">
-                <div className="carousel-item active">
-                    <img src={electronicsImg} className="d-block w-100" alt="electronics image"></img>
+        // <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+        //     <div className="carousel-inner">
+        //         <div className="carousel-item active">
+        //             <img src={electronicsImg} className="d-block w-100" alt="electronics image"></img>
+        //         </div>
+        //         <div className="carousel-item">
+        //             <img src={jewelryImg} className="d-block w-100" alt="jewelry"></img>
+        //         </div>
+        //         <div className="carousel-item">
+        //             <img src={placeholderImg} className="d-block w-100" alt="mens clothing"></img>
+        //         </div>
+        //         <div className="carousel-item">
+        //             <img src={placeholderImg} className="d-block w-100" alt="womens clothing"></img>
+        //         </div>
+        //     </div>
+        // </div>
+
+
+
+        <div id="carouselExampleFade" class="carousel slide carousel-fade">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src={electronicsImg} class="d-block w-100" alt="..."></img>
                 </div>
-                <div className="carousel-item">
-                    <img src={jewelryImg} className="d-block w-100" alt="jewelry"></img>
+                <div class="carousel-item">
+                    <img src={placeholderImg} class="d-block w-100" alt="..."></img>
                 </div>
-                <div className="carousel-item">
-                    <img src={placeholderImg} className="d-block w-100" alt="mens clothing"></img>
+                <div class="carousel-item">
+                    <img src={placeholderImg} class="d-block w-100" alt="..."></img>
                 </div>
-                <div className="carousel-item">
-                    <img src={placeholderImg} className="d-block w-100" alt="womens clothing"></img>
+                <div class="carousel-item">
+                    <img src={placeholderImg} class="d-block w-100" alt="..."></img>
                 </div>
             </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
         </div>
 
     );
