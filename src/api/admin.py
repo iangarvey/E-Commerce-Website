e8ef1b9d2f90ed0f01122a5d8cf253c87eb17aad
Ian Garvey <iangarvey@MacBook-Air-Ian.local>
2025-11-24 06:10:46 -0500

    adds admin view for order, orderItem

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User, Product, Category, Cart, CartItem
+from .models import db, User, Product, Category, Cart, CartItem, Order, OrderItem
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -16,6 +16,8 @@ def setup_admin(app):
     admin.add_view(ModelView(Category, db.session))
     admin.add_view(ModelView(Cart, db.session))
     admin.add_view(ModelView(CartItem, db.session))
+    admin.add_view(ModelView(Order, db.session))
+    admin.add_view(ModelView(OrderItem, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
